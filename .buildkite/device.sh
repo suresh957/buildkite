e894e948f2e6dcfbe9d95f2de29c7d0f7e8c2fd3
suresh957 <32921856+suresh957@users.noreply.github.com>
2019-08-23 10:59:14 +0530

    Update device.sh
    permissions modified

--- .buildkite/device.sh
+++ .buildkite/device.sh
@@ -1,6 +1,8 @@
 echo "buildkite checkout path is $BUILDKITE_BUILD_CHECKOUT_PATH"
-cd $BUILDKITE_BUILD_CHECKOUT_PATH
+cd $BUILDKITE_BUILD_CHECKOUT_PATH/.buildkite
 ls -a
+chmod 755 device.sh build.sh
+
 CART_LINK=$(buildkite-agent meta-data get cartlink)
 DEVICE=$(buildkite-agent meta-data get device)
 

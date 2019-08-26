bfb510b0a0434de7a6b0f5891df8f3f022a1e0b9
suresh957 <32921856+suresh957@users.noreply.github.com>
2019-08-26 14:44:01 +0530

    Update device.sh
    modified script

--- .buildkite/device.sh
+++ .buildkite/device.sh
@@ -10,14 +10,6 @@ echo "Details below!"
 echo $CART_LINK
 echo $DEVICE
 echo $BUILDKITE_AGENT_NAME
-
 sudo cat $BUILDKITE_BUILD_CHECKOUT_PATH/.buildkite/pipeline.yml
-sudo sed -i "s/cartlink-1/$CART_LINK/g" /etc/buildkite-agent/buildkite-agent.cfg
-git stash
-git checkout master
-git pull origin master
-sudo sed -i "s/cartlink: 1/$CART_LINK/g" $BUILDKITE_BUILD_CHECKOUT_PATH/.buildkite/pipeline.yml
-git add $BUILDKITE_BUILD_CHECKOUT_PATH/.buildkite/pipeline.yml
-git commit -m "msg"
-git push origin master
+
 

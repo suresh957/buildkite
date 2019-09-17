bcb3de8445f9ed53e259d8ac35df1d73ab087d0b
suresh957 <32921856+suresh957@users.noreply.github.com>
2019-09-17 16:15:01 +0530

    new file added

---
+++ raspi-flash-requirements.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#This sript will check and install all requirements related to flashing cartlinks.
+
+#check pip installed or not
+if pip --version 2>&1 > /dev/null ; then
+  echo "$(pip --version) is already installed"
+
+else
+  sudo apt-get install python-pip
+fi
+
+pip install -r requirements.txt
+
+#check adb tools installed or not
+if adb 2>&1 > /dev/null ; then
+  echo "ADB Tools are already installed"
+else
+  echo "ADB Tools are not installed, installing now!"
+  sudo apt-get update
+  sudo apt-get install -y android-tools-adb android-tools-fastboot
+fi
+
